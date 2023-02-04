d69059cabb3aad76204453c7183726bfa7638217
hothanh99 <thanhho@pavana.tech>
2023-02-04 13:09:17 +0700

    Add config and fix log path of tensorboardfor tl_training

--- D-ESCA_v2/core/Trainer/TL_Trainer.py
+++ D-ESCA_v2/core/Trainer/TL_Trainer.py
@@ -1,11 +1,33 @@
 import tensorflow as tf
 from core.Trainer import ModelTrainer
 from os.path import join
+from datetime import datetime
+from core.Preprocessing import Preprocessor
+from core.Postprocessing import Postprocessor
 
 class TL_Trainer(ModelTrainer):
     def __init__(self, cfg, from_config=True, **kwargs):
-        super().__init__(cfg)
+        # super().__init__(cfg)
+        self.model_name = cfg.MODEL.TYPE 
         self.log_dir = cfg.TRANSFER_LEARNING.SAVE_PATH
+        self.learning_rate = cfg.TRANSFER_LEARNING.LEARNING_RATE
+        self.epochs = cfg.TRANSFER_LEARNING.EPOCH
+        self._setup_logger()
+        self._setup_metrics()
+        self.pre_prc = Preprocessor(cfg)
+        self.post_prc = Postprocessor(cfg)
+
+        self.impl_steps = {
+            'train': self.train_step,
+            'test': self.test_step,
+            'val': self.val_step,
+        }
+        self.impl_logs = {
+            'train': self._write_train_log,
+            'test': self._write_test_log,
+            'val': self._write_train_log,
+        }
+
         self.theshold_save_path = join(self.log_dir,'save_parameter')
         self.beta = cfg.TRANSFER_LEARNING.BETA if from_config else kwargs['beta']
         self.training_anomaly = kwargs['training_anomaly']
@@ -17,7 +39,16 @@ class TL_Trainer(ModelTrainer):
         self.load_pretrained_weights(self.based_model_path)
         # tf.concat(temp, axis=0)
 
-    # override metrics
+    # # override metrics
+    # def _setup_logger(self):
+    #     # create tensorboard for each data part
+    #     log_name = datetime.now().strftime("%Y%m%d-%H%M%S")
+    #     self.tensorboard = {
+    #         'train': tf.summary.create_file_writer(join(self.log_dir, 'logs', log_name, 'train')),
+    #         'val': tf.summary.create_file_writer(join(self.log_dir, 'logs', log_name, 'val')),
+    #         'test': tf.summary.create_file_writer(join(self.log_dir, 'logs', log_name, 'test')),
+    #     }
+
     def _setup_metrics(self):
         self.trackers = {
             'total_loss': tf.keras.metrics.Mean(name="total_loss"),
