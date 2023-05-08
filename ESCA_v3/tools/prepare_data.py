6ed0731a3dad2229bdad175bc1f80f86b8955bcc
nqthinh493 <nqthinh493@gmail.com>
2023-05-08 16:35:50 +0900

    updata code

--- ESCA_v3/tools/prepare_data.py
+++ ESCA_v3/tools/prepare_data.py
@@ -1,33 +1,11 @@
-import os
-import sys
+import os, sys
 sys.path.append(os.getcwd())
-from core.DataLoader import Dataloader
-from helper.parser import arg_parser 
-from config import update_config, get_cfg_defaults
-import tensorflow as tf
+from config import autocfg as cfg
 
-# os.environ['CUDA_VISIBLE_DEVICES'] = "1"
+from multiprocessing import Pool
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from Preprocessor import SpectrogramExtractor
 
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-  # Restrict TensorFlow to only use the first GPU
-  try:
-    tf.config.set_logical_device_configuration(
-        gpus[0],
-        [tf.config.LogicalDeviceConfiguration(memory_limit=1024*3.5)])
-    logical_gpus = tf.config.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
-  except RuntimeError as e:
-    # Visible devices must be set before GPUs have been initialized
-    print(e)
 
-if __name__ == '__main__':
-    # update config based on default.yaml file
-    cfg = get_cfg_defaults()
-    # config_file = arg_parser('Create Dataloader for further uses.')
-    cfg = update_config(cfg, '.\config\params.yaml')
-
-    # initiate a feature_extractor with all parameters from cfg
-    data_loader = Dataloader(cfg)
-    data_loader.create_tfrecord()
-    data_loader.accumulate_stat()
+featureExtractor = SpectrogramExtractor(filter_cfg=cfg.GAMMATONE_SETTING)
+featureExtractor.export_tfrecord()
